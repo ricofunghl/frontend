1aec7814951a4c7df1a7b9a88e67edde52fdb50b
Hok Leung Rico Fung <hfung4@nait.ca>
2019-10-23 16:15:35 -0600

    added material-ui

---
+++ src/components/Base/Base.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import AppBar from '../AppBar/AppBar';
+
+class Base extends Component {
+    render () {
+        return (
+            <AppBar />
+        )
+    }
+}
+
+export default Base;
